e5a6e3c340726ce0b501d237fd1ea4168b14569f
chilakamarthinavya <chilakamarthinavya@gmail.com>
2025-12-25 19:56:05 +0530

    taerraform jenkins

---
+++ 60-acm/variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+
+variable "zone_id" {
+    default = "Z02392793SKM26KAZOWED"
+  
+}
+
+variable "zone_name" {
+    default = "latha.site"
+  
+}
---
+++ 70-frontend-alb/provider.tf
@@ -0,0 +1,41 @@
+#terraform {
+  #required_providers {
+    #aws = {
+      #source  = "hashicorp/aws"
+      #version = "5.98.0"
+    #}
+  #}
+  #backend "s3" {
+    #bucket       = "navya-remote-state-dev"
+    #key          = "roboshop-dev-sg"
+    #region       = "us-east-1"
+    #encrypt      = true
+    #use_lockfile = true
+  #}
+#}
+#provider "aws" {
+  #region = "us-east-1"
+  # Configuration options
+#}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "navya-remote-state-dev"
+    key    = "roboshop-dev-frontend-alb"
+    region = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
